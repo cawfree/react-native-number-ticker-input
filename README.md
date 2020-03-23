e46cb9d697cbe2a28140bccc672185d90f8a4e0c
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2020-03-23 09:44:31 +0000

    Published to GitHub! 🚀

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # react-native-number-ticker-input
-A numeric input in the style of a ticker.
+A numeric input in the style of a ticker, which supports [Android](https://developer.android.com/), [iOS](https://developer.apple.com/) and [Web](https://github.com/necolas/react-native-web).
+
+## Getting Started
+
+Using [`npm`]():
+
+```bash
+npm install --save react-native-number-ticker-input
+```
+
+Using [`yarn`]():
+
+```bash
+yarn add react-native-number-ticker-input
+```
+
+## Usage
+
+The component works very similarly to a [`<TextInput/>`](https://reactnative.dev/docs/textinput), since it is after all, just an animated wrapper of one!
+
+```javascript
+import React, { useState } from "react";
+import NumberTickerInput from "react-native-number-ticker-input";
+
+export default () => {
+  const [value, setValue] = useState(0);
+  return (
+    <NumberTickerInput
+      value={value}
+      onChange={setValue}
+    />
+  );
+};
+```
+
+## Props
+
+Property | Type | Required | Default value | Description
+:--- | :--- | :--- | :--- | :---
+height|number|no|100| Defines the component height.
+value|number|no|0| Defines the current value.
+onChange|func|no|0| Callback to update the value.
+significantDigits|number|no|4| The number of digits to show before the decimal point.
+significantContainerStyle|style|no|undefined| The style of the outer container for a significant digit.
+significantTickerStyle|style|no|undefined| The style of an individual number ticker for a significant digit.
+
+decimalDigits|number|no|2| The number of decimal digits.
+decimalContainerStyle|style|no|undefined| The style of the outer container for a decimal digit.
+decimalTickerStyle|style|no|undefined| The style of an individual number ticker for a decimal digit.
+DecimalPoint|elementType|no|DecimalPoint|The component to use when rendering the decimal point. This will not be rendered if decimalDigits === 0.
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,166 @@
+import React, { useState, useEffect } from "react";
+import {
+  Platform,
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  Animated,
+  Easing,
+} from "react-native";
+import PropTypes from "prop-types";
+
+const styles = StyleSheet.create({
+  invisible: { opacity: 0 },
+  flex: { flex: 1, flexDirection: "row" },
+  centerChildren: { alignItems: "center", justifyContent: "center" },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "black",
+  },
+  overflowHidden: { overflow: "hidden" },
+});
+
+const DecimalPoint = ({ ...extraProps }) => (
+  <View style={[styles.flex, styles.centerChildren]}>
+    <View style={[styles.dot, styles.overflowHidden]} />
+  </View>
+);
+
+const Ticker = ({ value, height, containerStyle, style, ...extraProps }) => {
+  const [animOffset] = useState(
+    new Animated.Value(-Number.parseInt(9) * height)
+  );
+  useEffect(
+    () =>
+      Animated.timing(animOffset, {
+        easing: Easing.inOut(Easing.quad),
+        toValue: -Number.parseInt(value) * height,
+        duration: 200,
+        useNativeDriver: Platform.OS !== "web",
+      }).start() && undefined,
+    [animOffset, value]
+  );
+  return (
+    <View style={[styles.flex, containerStyle]}>
+      <Animated.View
+        style={[
+          {
+            transform: [{ translateY: animOffset }],
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "center",
+          },
+        ]}
+      >
+        <View>
+          {[...Array(10)].map((_, i) => (
+            <View style={[styles.centerChildren, { height }]}>
+              <Text style={style} children={`${i}`} />
+            </View>
+          ))}
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+// https://stackoverflow.com/a/25198989/1701465
+const applyZeroPadding = (input, length) =>
+  (Array(length + 1).join("0") + input).slice(-length);
+
+const NumberTickerInput = ({
+  style,
+  height,
+  value,
+  onChange,
+  significantDigits,
+  significantContainerStyle,
+  significantTickerStyle,
+  decimalContainerStyle,
+  decimalTickerStyle,
+  decimalDigits,
+  DecimalPoint,
+  ...extraProps
+}) => {
+  console.log(value);
+  const digits = applyZeroPadding(
+    value,
+    significantDigits + decimalDigits
+  ).split("");
+  return (
+    <View style={[{ height }, styles.overflowHidden]} pointerEvents="box-none">
+      <TextInput
+        {...extraProps}
+        selectTextOnFocus
+        keyboardType="numeric"
+        style={[StyleSheet.absoluteFill, styles.invisible]}
+        value={value}
+        onChangeText={(e) =>
+          onChange(
+            Number.parseInt(
+              applyZeroPadding(e, significantDigits + decimalDigits).substring(
+                0,
+                significantDigits + decimalDigits
+              )
+            )
+          )
+        }
+        maxLength={significantDigits + decimalDigits}
+        selectionColor="transparent"
+      />
+      <View pointerEvents="none" style={styles.flex}>
+        {digits.slice(0, significantDigits).map((value, i) => (
+          <Ticker
+            containerStyle={significantContainerStyle}
+            style={significantTickerStyle}
+            key={i}
+            height={height}
+            value={value}
+          />
+        ))}
+        {decimalDigits > 0 && (
+          <>
+            <DecimalPoint />
+            {digits.slice(significantDigits).map((value, i) => (
+              <Ticker
+                containerStyle={decimalContainerStyle}
+                style={decimalTickerStyle}
+                key={i}
+                height={height}
+                value={value}
+              />
+            ))}
+          </>
+        )}
+      </View>
+    </View>
+  );
+};
+
+NumberTickerInput.propTypes = {
+  ...View.propTypes,
+  height: PropTypes.number,
+  value: PropTypes.number,
+  onChange: PropTypes.func,
+  significantDigits: PropTypes.number,
+  significantContainerStyle: View.propTypes.style,
+  significantTickerStyle: View.propTypes.style,
+  decimalContainerStyle: View.propTypes.style,
+  decimalTickerStyle: View.propTypes.style,
+  decimalDigits: PropTypes.number,
+  DecimalPoint: PropTypes.elementType,
+};
+
+NumberTickerInput.defaultProps = {
+  style: styles.flex,
+  height: 100,
+  value: 0,
+  significantDigits: 4,
+  decimalDigits: 2,
+  DecimalPoint,
+};
+
+export default NumberTickerInput;
